5d75b1c4d02c7edbd97d173534d031dc0cdc4f74
oleg <legik7127@gmail.com>
2022-11-06 14:26:12 +0600

    Changed Combinations

--- src/main/java/functions_and_recurtions/Combinations.java
+++ src/main/java/functions_and_recurtions/Combinations.java
@@ -1,18 +1,18 @@
-package functions_and_recurtions;
-
-public class Combinations {
-    class Solution {
-        public List<List<Integer>> combine(int n, int k) {
-            List<List<Integer>> ans = new ArrayList<>();
-            Integer[] comb = new Integer[k];
-            ans.add(Arrays.asList(comb));
-            if (n == 0) {
-                return ans;
-            }
-            else {
-                return combine(n-1, k);
-            }
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Integer[] comb = new Integer[k];
+        //comb.add(0, 0);
+        //comb.add(Integer.valueOf(n, k)) // Необходимо сделать запись в comb,
+        ans.add(Arrays.asList(comb));
+        if (n == 0) {
+            return ans;
         }
+        else {
+            return combine(n-1, k);
+        }
+        // else if{
+        //    return combine(n-1, k-1);
+        // }
     }
-
-
+}
