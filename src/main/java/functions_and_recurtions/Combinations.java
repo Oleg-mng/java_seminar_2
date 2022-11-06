b4d6c2b72386ef761b04f43ab971099b42895cd7
oleg <legik7127@gmail.com>
2022-11-06 13:19:11 +0600

    Changed Combinations

--- src/main/java/functions_and_recurtions/Combinations.java
+++ src/main/java/functions_and_recurtions/Combinations.java
@@ -1,4 +1,12 @@
 package functions_and_recurtions;
 
 public class Combinations {
-}
+    public static void main (String[] args) {
+            List<List<Integer>> ans = new ArrayList<>();
+            Integer[] comb = new Integer[k];
+            ans.add(Arrays.asList(comb));
+        }
+    }
+
+
+
