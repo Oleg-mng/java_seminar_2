90cb8ef1d26c761c7a81ce7023554417b253a93b
oleg <legik7127@gmail.com>
2022-11-06 13:30:26 +0600

    Changed Combinations

--- src/main/java/functions_and_recurtions/Combinations.java
+++ src/main/java/functions_and_recurtions/Combinations.java
@@ -1,12 +1,18 @@
 package functions_and_recurtions;
 
 public class Combinations {
-    public static void main (String[] args) {
+    class Solution {
+        public List<List<Integer>> combine(int n, int k) {
             List<List<Integer>> ans = new ArrayList<>();
             Integer[] comb = new Integer[k];
             ans.add(Arrays.asList(comb));
+            if (n == 0) {
+                return ans;
+            }
+            else {
+                return combine(n-1, k);
+            }
         }
     }
-
 
 
